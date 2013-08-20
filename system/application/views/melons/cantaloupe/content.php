aa3c613aee5570f8ed9afa7d0f40adf74ada8e13
Craig Dietrich <craigdietrich@gmail.com>
2013-08-20 12:00:18 -0700

    Test weather a view exists before trying to load it
    Ie, remove show_404() as the behavior, this way "plain" view can be
    sent through from the controller without a problem

--- system/application/views/melons/cantaloupe/content.php
+++ system/application/views/melons/cantaloupe/content.php
@@ -10,5 +10,8 @@
 <?$this->template->add_js(path_from_file(__FILE__).'js/scalarhelp.jquery.js')?>
 <?$this->template->add_js(path_from_file(__FILE__).'js/scalarstructuredgallery.jquery.js')?>
 <?$this->template->add_js(path_from_file(__FILE__).'js/scalarpinwheel.jquery.js')?>
-<?if (!file_exists(confirm_slash(APPPATH).'views/melons/honeydew/content/'.$view.'.php')) show_404()?>
-<?$this->load->view('melons/cantaloupe/'.$view)?>
+<?
+if (file_exists(confirm_slash(APPPATH).'views/melons/cantaloupe/'.$view.'.php')) {
+  $this->load->view('melons/cantaloupe/'.$view);
+}
+?>
