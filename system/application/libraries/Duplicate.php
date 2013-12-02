c31a128191958efd59e2a8b369ce3e72b81dce85
ANVC <Alliance4NVC@gmail.com>
2013-12-01 21:03:52 -0800

    Duplication: Complete page and version scrubbing (fully operational except for relational data)

--- system/application/libraries/Duplicate.php
+++ system/application/libraries/Duplicate.php
@@ -47,11 +47,32 @@ class Duplicate {
  		if (empty($duplicated_book_id)) throw new Exception('Invalid book ID');
     	$user_id =@ (int) $array['user_id'];
     	
-    	$book = $this->_scrub_book_fields($this->CI->books->get($duplicated_book_id), $user_id);
+    	$duplicated_book = $this->CI->books->get($duplicated_book_id);
     	
     	// Create book
+    	$book 		= $this->_scrub_book_fields($duplicated_book, $user_id);
 	 	$book_id 	= $this->CI->books->add($book);
 	 	$book 		= $this->CI->books->get($book_id);
+	 	
+	 	// Old and new paths and URLs
+	 	$duplicated_uri = confirm_slash(base_url()).$duplicated_book->slug;
+	 	$duplicated_path = confirm_slash(FCPATH).$duplicated_book->slug;
+	 	$uri = confirm_slash(base_url()).$book->slug;
+	 	$path = confirm_slash(FCPATH).$book->slug;
+	 	
+	 	// Copy thumbnail and background images (if applicable)
+	 	$book->thumbnail  = '';
+	 	$book->background = '';
+	 	if (!empty($duplicated_book->thumbnail) && file_exists($duplicated_path.'/'.$duplicated_book->thumbnail)) {
+	 		if (copy($duplicated_path.'/'.$duplicated_book->thumbnail, $path.'/'.$duplicated_book->thumbnail)) {	
+	 			$book->thumbnail = $duplicated_book->thumbnail;
+	 		}
+	 	}
+    	 if (!empty($duplicated_background) && file_exists($duplicated_path.'/'.$duplicated_book->background)) {
+	 		if (copy($duplicated_path.'/'.$duplicated_book->background, $path.'/'.$duplicated_book->background)) {	
+	 			$book->background = $duplicated_book->background;
+	 		}
+	 	}	 	
 
 	 	// Copy pages
 	 	$pages = $this->CI->pages->get_all($duplicated_book_id);  // Only live pages
@@ -63,15 +84,12 @@ class Duplicate {
 	 		$content_id = $this->CI->pages->create($page);
 	 		// Save new version
 	 		$version = (is_array($version)) ? $version[0] : $version;
-	 		$version = $this->_scrub_version_fields($version, $content_id);
+	 		$version = $this->_scrub_version_fields($version, $content_id, array('prev_uri'=>$duplicated_uri,'uri'=>$uri,'prev_title'=>$duplicated_book->title));
 	 		$version_id = $this->CI->versions->create($content_id, $version);
-	 		// Save relate
-	 		// TODO
 	 	}	 	
-
-		//die('In library');
 	 	
-	 	// Copy thumbnail and background images (if applicable)
+	 	// Save relate (after all the pages are already created)
+	 	// TODO	 	
     	
     	return $book_id;
     	
@@ -108,25 +126,50 @@ class Duplicate {
     	
     }
     
-    private function _scrub_version_fields($obj, $content_id=0) {
+    private function _scrub_version_fields($obj, $content_id=0, $options=array()) {
     	
     	$ns_array = $this->CI->config->item('namespaces');
-    	$array = (array) $obj;
+    	$array = (array) $obj;  // RDF fields have colons in them, invalid as object field names
     	
+    	// Scrub fields
 		if (isset($array['version_id'])) unset($array['version_id']);
     	if (isset($array['created'])) unset($array['created']);
     	if (isset($array['urn'])) unset($array['urn']);	
     	$array['user_id'] = (isset($array['user'])) ? (int) $array['user'] : 0;	
     	$array['content_id'] = (int) $content_id;
     	
+    	// Flatten RDF fields in the array, then remove the RDF field
 	 	if (isset($array['rdf'])) { 
 		 	foreach ($array['rdf'] as $field => $value) {
 		 		$field = toNS($field, $ns_array);
 		 		$array[$field] = $value;
 		 	}
 	 	}
-	 	unset($array['rdf']);    	
-	 	
+	 	unset($array['rdf']);    
+
+	 	// URLs
+	 	if (isset($array['url']) && !empty($array['url']) && isset($options['prev_uri']) && isset($options['uri'])) {
+	 		$has_transcribed = false;
+	 		// Hard URL
+	 		if ($options['prev_uri'] == substr($array['url'], 0, strlen($options['prev_uri']))) {
+	 			$array['url'] = str_replace($options['prev_uri'], $options['uri'], $array['url']);
+	 			$has_transcribed = true;
+	 		}
+	 		// Soft URL
+	 		elseif (!isURL($array['url'])) {
+	 			$array['url'] = confirm_slash($options['prev_uri']).$array['url'];  // Set to previous book's URI, to avoid duplicating media
+	 			$has_transcribed = true;	
+	 		}
+	 		// Update RDF dcterms:source with title of previous book
+	 		if (isset($options['prev_title']) && $has_transcribed) {
+	 			if (!isset($array['dcterms:source'])) $array['dcterms:source'] = array();
+	 			$array['dcterms:source'][] = array('value'=>strip_tags($options['prev_title']),'type'=>'literal'); 
+	 		}
+	 	}
+
+ 		// Text content: remove hard URLs in resource=""
+	 	$array['content'] = str_replace('resource="'.confirm_slash($options['prev_uri']), 'resource="', $array['content']);
+
 	 	return $array;
     
     }
