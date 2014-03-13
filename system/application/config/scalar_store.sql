83b6b84bc2d6fea6bd8b2336e46931dea8aaa5b4
ANVC <alliance4nvc@gmail.com>
2014-03-13 09:43:12 -0700

    Updated SQL file

--- system/application/config/scalar_store.sql
+++ system/application/config/scalar_store.sql
@@ -1,13 +1,3 @@
---
--- Database: `scalar_store`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_books`
---
-
 CREATE TABLE IF NOT EXISTS `scalar_db_books` (
   `book_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -29,13 +19,7 @@ CREATE TABLE IF NOT EXISTS `scalar_db_books` (
   `user` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`book_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_content`
---
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
 CREATE TABLE IF NOT EXISTS `scalar_db_content` (
   `content_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -55,13 +39,7 @@ CREATE TABLE IF NOT EXISTS `scalar_db_content` (
   `created` datetime NOT NULL,
   PRIMARY KEY (`content_id`),
   KEY `book_id` (`book_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_rel_annotated`
---
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
 CREATE TABLE IF NOT EXISTS `scalar_db_rel_annotated` (
   `parent_version_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -74,12 +52,6 @@ CREATE TABLE IF NOT EXISTS `scalar_db_rel_annotated` (
   KEY `parent_child` (`parent_version_id`,`child_version_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_rel_contained`
---
-
 CREATE TABLE IF NOT EXISTS `scalar_db_rel_contained` (
   `parent_version_id` int(10) unsigned NOT NULL DEFAULT '0',
   `child_version_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -87,24 +59,12 @@ CREATE TABLE IF NOT EXISTS `scalar_db_rel_contained` (
   KEY `parent_child` (`parent_version_id`,`child_version_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_rel_referenced`
---
-
 CREATE TABLE IF NOT EXISTS `scalar_db_rel_referenced` (
   `parent_version_id` int(10) unsigned NOT NULL,
   `child_version_id` int(10) unsigned NOT NULL DEFAULT '0',
   `reference_text` varchar(255) DEFAULT NULL,
   KEY `parent_child` (`parent_version_id`,`child_version_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_rel_replied`
---
 
 CREATE TABLE IF NOT EXISTS `scalar_db_rel_replied` (
   `parent_version_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -114,35 +74,17 @@ CREATE TABLE IF NOT EXISTS `scalar_db_rel_replied` (
   KEY `parent_child` (`parent_version_id`,`child_version_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_rel_tagged`
---
-
 CREATE TABLE IF NOT EXISTS `scalar_db_rel_tagged` (
   `parent_version_id` int(10) unsigned NOT NULL,
   `child_version_id` int(10) unsigned NOT NULL DEFAULT '0',
   KEY `parent_child` (`parent_version_id`,`child_version_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_resources`
---
-
 CREATE TABLE IF NOT EXISTS `scalar_db_resources` (
   `field` varchar(64) NOT NULL,
   `value` mediumtext NOT NULL,
   KEY `field` (`field`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_sessions`
---
 
 CREATE TABLE IF NOT EXISTS `scalar_db_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
@@ -154,12 +96,6 @@ CREATE TABLE IF NOT EXISTS `scalar_db_sessions` (
   KEY `last_activity_idx` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_users`
---
-
 CREATE TABLE IF NOT EXISTS `scalar_db_users` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -169,13 +105,7 @@ CREATE TABLE IF NOT EXISTS `scalar_db_users` (
   `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `is_super` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_user_books`
---
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
 CREATE TABLE IF NOT EXISTS `scalar_db_user_books` (
   `user_id` int(10) unsigned NOT NULL DEFAULT '0',
@@ -188,12 +118,6 @@ CREATE TABLE IF NOT EXISTS `scalar_db_user_books` (
   KEY `user_book` (`user_id`,`book_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_user_history`
---
-
 CREATE TABLE IF NOT EXISTS `scalar_db_user_history` (
   `history_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
@@ -202,13 +126,7 @@ CREATE TABLE IF NOT EXISTS `scalar_db_user_history` (
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`history_id`),
   KEY `user_id` (`user_id`,`content_id`,`book_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_versions`
---
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
 
 CREATE TABLE IF NOT EXISTS `scalar_db_versions` (
   `version_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -227,110 +145,9 @@ CREATE TABLE IF NOT EXISTS `scalar_db_versions` (
   PRIMARY KEY (`version_id`),
   KEY `content_id` (`content_id`),
   KEY `version_num` (`version_num`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_db_whitelist`
---
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
 
 CREATE TABLE IF NOT EXISTS `scalar_db_whitelist` (
   `book_id` int(10) unsigned NOT NULL,
   `domain` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_store_g2t`
---
-
-CREATE TABLE IF NOT EXISTS `scalar_store_g2t` (
-  `g` mediumint(8) unsigned NOT NULL,
-  `t` mediumint(8) unsigned NOT NULL,
-  UNIQUE KEY `gt` (`g`,`t`),
-  KEY `tg` (`t`,`g`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci DELAY_KEY_WRITE=1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_store_id2val`
---
-
-CREATE TABLE IF NOT EXISTS `scalar_store_id2val` (
-  `id` mediumint(8) unsigned NOT NULL,
-  `misc` tinyint(1) NOT NULL DEFAULT '0',
-  `val` text COLLATE utf8_unicode_ci NOT NULL,
-  `val_type` tinyint(1) NOT NULL DEFAULT '0',
-  UNIQUE KEY `id` (`id`,`val_type`),
-  KEY `v` (`val`(64))
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci DELAY_KEY_WRITE=1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_store_o2val`
---
-
-CREATE TABLE IF NOT EXISTS `scalar_store_o2val` (
-  `id` mediumint(8) unsigned NOT NULL,
-  `misc` tinyint(1) NOT NULL DEFAULT '0',
-  `val_hash` char(32) COLLATE utf8_unicode_ci NOT NULL,
-  `val` text COLLATE utf8_unicode_ci NOT NULL,
-  UNIQUE KEY `id` (`id`),
-  KEY `vh` (`val_hash`),
-  KEY `v` (`val`(64))
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci DELAY_KEY_WRITE=1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_store_s2val`
---
-
-CREATE TABLE IF NOT EXISTS `scalar_store_s2val` (
-  `id` mediumint(8) unsigned NOT NULL,
-  `misc` tinyint(1) NOT NULL DEFAULT '0',
-  `val_hash` char(32) COLLATE utf8_unicode_ci NOT NULL,
-  `val` text COLLATE utf8_unicode_ci NOT NULL,
-  UNIQUE KEY `id` (`id`),
-  KEY `vh` (`val_hash`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci DELAY_KEY_WRITE=1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_store_setting`
---
-
-CREATE TABLE IF NOT EXISTS `scalar_store_setting` (
-  `k` char(32) COLLATE utf8_unicode_ci NOT NULL,
-  `val` text COLLATE utf8_unicode_ci NOT NULL,
-  UNIQUE KEY `k` (`k`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci DELAY_KEY_WRITE=1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scalar_store_triple`
---
-
-CREATE TABLE IF NOT EXISTS `scalar_store_triple` (
-  `t` mediumint(8) unsigned NOT NULL,
-  `s` mediumint(8) unsigned NOT NULL,
-  `p` mediumint(8) unsigned NOT NULL,
-  `o` mediumint(8) unsigned NOT NULL,
-  `o_lang_dt` mediumint(8) unsigned NOT NULL,
-  `o_comp` char(35) COLLATE utf8_unicode_ci NOT NULL,
-  `s_type` tinyint(1) NOT NULL DEFAULT '0',
-  `o_type` tinyint(1) NOT NULL DEFAULT '0',
-  `misc` tinyint(1) NOT NULL DEFAULT '0',
-  UNIQUE KEY `t` (`t`),
-  KEY `sp` (`s`,`p`),
-  KEY `os` (`o`,`s`),
-  KEY `po` (`p`,`o`),
-  KEY `misc` (`misc`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci DELAY_KEY_WRITE=1;
-
