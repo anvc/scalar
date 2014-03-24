583e97e30e17d4f3f451b46b51ced8f7e7ffbab2
Olivier <o@kojilab.com>
2014-03-24 13:47:17 -0700

    loading message atop results table

--- system/application/views/melons/cantaloupe/js/scalarindex.jquery.js
+++ system/application/views/melons/cantaloupe/js/scalarindex.jquery.js
@@ -1,21 +1,21 @@
 /**
- * Scalar    
+ * Scalar
  * Copyright 2013 The Alliance for Networking Visual Culture.
  * http://scalar.usc.edu/scalar
  * Alliance4NVC@gmail.com
  *
- * Licensed under the Educational Community License, Version 2.0 
- * (the "License"); you may not use this file except in compliance 
+ * Licensed under the Educational Community License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
- * http://www.osedu.org/licenses /ECL-2.0 
- * 
+ *
+ * http://www.osedu.org/licenses /ECL-2.0
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an "AS IS"
  * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing
- * permissions and limitations under the License.       
- */  
+ * permissions and limitations under the License.
+ */
 
 ;(function( $, window, document, undefined ) {
 
@@ -23,17 +23,17 @@
 		defaults = {
 			root_url: ''
 		};
-	
+
 	/**
 	 * Manages the index dialog.
 	 */
 	function ScalarIndex( element, options ) {
-	
+
 		this.element = $(element);
 		this.options = $.extend( {}, defaults, options );
 		this._defaults = defaults;
 		this._name = pluginName;
-		
+
 		this.DisplayMode = {
 			Path: 'Path',
 			Page: 'Page',
@@ -42,11 +42,11 @@
 			Annotation: 'Annotation',
 			Comment: 'Reply'
 		}
-		
+
 		this.init();
-		
+
 	}
-	
+
 	ScalarIndex.prototype.bodyContent = null;			// body content container
 	ScalarIndex.prototype.currentMode = null;			// Current display mode
 	ScalarIndex.prototype.currentPage = null;			// Current page of results being displayed
@@ -56,16 +56,16 @@
 	ScalarIndex.prototype.controlBar = null;			// Index controls
 	ScalarIndex.prototype.firstRun = null;				// First time the plug-in has run?
 	ScalarIndex.prototype.maxPages = null;				// Maximum number of pages with known results in the current tab
-	
+
 	ScalarIndex.prototype.init = function () {
-	
+
 		var me = this;
-	
+
 		this.currentPage = 1;
 		this.maxPages = 1;
 		this.resultsPerPage = 10;
 		this.firstRun = true;
-		
+
 		this.element.addClass( 'modal fade' );
 		this.element.attr( {
 			'tabindex': '-1',
@@ -77,7 +77,7 @@
 		var modalContent = this.element.find( '.modal-content' );
 		var header = $( '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h2 class="modal-title heading_font">Index</h2></div>' ).appendTo( modalContent );
 		this.bodyContent = $( '<div class="modal-body"></div>' ).appendTo( modalContent );
-	
+
 		this.controlBar = $( '<ul class="nav nav-pills"></ul>' ).appendTo( this.bodyContent );
 		var pathBtn = $( '<li id="pathBtn" class="active caption_font"><a href="javascript:;">Paths</li>' ).appendTo( this.controlBar );
 		var pageBtn = $( '<li id="pageBtn" class="caption_font"><a href="javascript:;">Pages</li>' ).appendTo( this.controlBar );
@@ -85,44 +85,46 @@
 		var tagBtn = $( '<li id="tagBtn" class="caption_font"><a href="javascript:;">Tags</li>' ).appendTo( this.controlBar );
 		var annotationBtn = $( '<li id="annotationBtn" class="caption_font"><a href="javascript:;">Annotations</li>' ).appendTo( this.controlBar );
 		var commentBtn = $( '<li id="replyBtn" class="caption_font"><a href="javascript:;">Comments</li>' ).appendTo( this.controlBar );
-		
+
 		pathBtn.click(function () { me.setDisplayMode( me.DisplayMode.Path ); });
 		pageBtn.click(function () { me.setDisplayMode( me.DisplayMode.Page ); });
 		mediaBtn.click(function () { me.setDisplayMode( me.DisplayMode.Media ); });
 		tagBtn.click(function () { me.setDisplayMode( me.DisplayMode.Tag ); });
 		annotationBtn.click(function () { me.setDisplayMode( me.DisplayMode.Annotation ); });
 		commentBtn.click(function () { me.setDisplayMode( me.DisplayMode.Comment ); });
-		
+
 		var resultsDiv = $( '<div class="results_list caption_font"></div>' ).appendTo( this.bodyContent );
-		this.resultsTable = $( '<table></table>' ).appendTo( resultsDiv );
-		
+		this.resultsTable = $( '<table class="table table-striped"></table>' ).appendTo( resultsDiv );
+		this.loading = $('<div class="loading"><p>Loading...</p></div>').hide().insertAfter(this.resultsTable);
+
 		this.pagination = $( '<ul class="pagination caption_font"></ul>' ).appendTo( this.bodyContent );
-		
+
+
 	}
-	
+
 	ScalarIndex.prototype.showIndex = function() {
-	
+
 		this.element.modal().on('hidden.bs.modal', function (e) {
 			$( 'body' ).css( 'overflowY', 'auto' );
 		});
-		
+
 		if ( this.firstRun ) {
 			this.setDisplayMode(this.DisplayMode.Path);
 			this.firstRun = false;
 		}
 		setState( ViewState.Modal );
 	}
-	
+
 	ScalarIndex.prototype.hideIndex = function() {
-	
+
 		this.element.modal( 'hide' );
 		restoreState();
 	}
-	
+
 	ScalarIndex.prototype.setDisplayMode = function( mode ) {
-	
+
 		var me = this;
-	
+
 		if ( this.currentMode != mode ) {
 			this.currentMode = mode;
 			this.currentPage = 1;
@@ -132,33 +134,42 @@
 			$('#'+mode+'Btn').addClass('active');
 			me.getResults();
 		}
-		
+
 	}
-	
+
+	ScalarIndex.prototype.showLoading = function() {
+		var h = this.resultsTable.height();
+		var $p = this.loading.find('p');
+		this.loading.width(this.resultsTable.width()).height(h);
+		$p.css('top', (((h-$p.height())/2)-5)+'px');
+		this.loading.show();
+	}
+
 	ScalarIndex.prototype.getResults = function() {
 		var me = this;
-		scalarapi.loadNodesByType( 
-			this.currentMode.toLowerCase(), true, 
-			function( data ) { me.handleResults( data ); }, 
-			null, 0, false, null, ( me.currentPage - 1 ) * me.resultsPerPage, me.resultsPerPage 
+		this.showLoading();
+		scalarapi.loadNodesByType(
+			this.currentMode.toLowerCase(), true,
+			function( data ) { me.loading.hide(); me.handleResults( data ); },
+			null, 0, false, null, ( me.currentPage - 1 ) * me.resultsPerPage, me.resultsPerPage
 		);
 	}
-	
+
 	ScalarIndex.prototype.handleResults = function( data ) {
-		
+
 		var i, node, description, row, prev, next,
 			nodes = [],
 			me = this;
-		
+
 		for ( i in data ) {
 			node = scalarapi.getNode( i );
 			if ( node !== undefined ) {
 				nodes.push( node );
 			}
 		}
-		
+
 		this.resultsTable.empty();
-		
+
 		for ( i in nodes ) {
 			node = nodes[i];
 			description = node.current.description;
@@ -171,11 +182,11 @@
 			row.data( 'node', node );
 			row.click( function() { document.location = addTemplateToURL($(this).data('node').url, 'cantaloupe'); } );
 		}
-		
+
 		if ( nodes.length == 0 ) {
 			row = $( '<tr><td style="width:30%">No results found.</td><td></td></tr>' ).appendTo( this.resultsTable );
 		}
-		
+
 		this.pagination.empty();
 		if (( nodes.length == this.resultsPerPage ) || ( this.currentPage > 1 )) {
 			if ( this.currentPage > 1 ) {
@@ -201,27 +212,27 @@
 				next = $( '<li class="disabled"><a href="javascript:;">&raquo;</a></li>' ).appendTo( this.pagination );
 			}
 		}
-		
+
 	}
-	
+
 	ScalarIndex.prototype.previousPage = function() {
 		if ( this.currentPage > 1) {
 			this.currentPage--;
 			this.getResults();
 		}
 	}
-	
+
 	ScalarIndex.prototype.nextPage = function() {
 		this.currentPage++;
 		this.maxPages = Math.max( this.maxPages, this.currentPage );
 		this.getResults();
 	}
-	
+
 	ScalarIndex.prototype.goToPage = function( pageNum ) {
 		this.currentPage = pageNum;
 		this.getResults();
 	}
-			
+
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if ( !$.data(this, "plugin_" + pluginName )) {
