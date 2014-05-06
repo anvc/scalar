ec64d2bda1c58886b461d025dd2d5e0bbb634ed3
ANVC <alliance4nvc@gmail.com>
2014-05-06 13:25:08 -0700

    Authors didn't have permission to edit user rows in Dashboard > Book Users after a recent permissions update

--- system/application/controllers/system.php
+++ system/application/controllers/system.php
@@ -679,7 +679,7 @@ class System extends MY_Controller {
 				$this->data['book'] = $this->books->get($book_id);
 				$this->set_user_book_perms();
 				if ('users'==$section) {
-					if (!$this->data['login_is_super']) die ('{"error":"Invalid permissions to edit user"}');
+					if (!$this->login_is_book_admin() && !$this->data['login_is_super']) die ('{"error":"Invalid permissions to edit user"}');
 				} elseif ('pages'==$section) {
 					if (!$this->login_is_book_admin() && !$this->pages->is_owner($this->data['login']->user_id,$id)) die ('{"error":"Invalid permissions"}');		
 				} elseif ('books'==$section) {
