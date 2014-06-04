6c11794a3072197212b2f427a6ad38bbc535ac43
ANVC <alliance4nvc@gmail.com>
2014-06-04 15:17:09 -0700

    Complete 'Replace media: copy relationships'

--- system/application/views/melons/honeydew/content/upload.php
+++ system/application/views/melons/honeydew/content/upload.php
@@ -26,7 +26,7 @@ function insert_rel_fields(current_urn, current_slug) {
   var insert_into = $('#relations');
   insert_into.empty();
   var fields = [];
-  $.getJSON(current_uri+'.rdfjson?ref=1&rec=1', function( data ) {
+  $.getJSON(current_uri+'.rdfjson?ref=1&rec=1', function(data) {
     for (var s in data) {
       // Paths
       if (-1!=s.indexOf('urn:scalar:path')) {
@@ -34,21 +34,60 @@ function insert_rel_fields(current_urn, current_slug) {
         	fields.push({name:'container_of',value:get_urn_from_uri(data,data[s]['http://www.openannotation.org/ns/hasTarget'][0].value)});
         } else {
         	fields.push({name:'has_container',value:get_urn_from_uri(data,data[s]['http://www.openannotation.org/ns/hasBody'][0].value)});
-        	// num
+        	var path_index = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#index=')+7);
+        	fields.push({name:'has_container_sort_number',value:path_index});       	
         }
       }      
       // Replies
       if (-1!=s.indexOf('urn:scalar:reply')) {
         if (current_uri==data[s]['http://www.openannotation.org/ns/hasBody'][0].value) {
         	fields.push({name:'reply_of',value:get_urn_from_uri(data,data[s]['http://www.openannotation.org/ns/hasTarget'][0].value)});
-        	// Paragraph num
+        	fields.push({name:'reply_of_paragraph_num',value:0});
+        	var datetime = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#datetime=')+10);
+        	datetime = datetime.replace('T',' ');
+        	datetime = datetime.substr(0, datetime.indexOf('+'));
+        	fields.push({name:'reply_of_datetime',value:datetime});         	
         } else {
         	fields.push({name:'has_reply',value:get_urn_from_uri(data,data[s]['http://www.openannotation.org/ns/hasBody'][0].value)});
-        	// Paragraph num
-        	// Datetime
+        	fields.push({name:'has_reply_paragraph_num',value:0});
+        	var datetime = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#datetime=')+10);
+        	datetime = datetime.replace('T',' ');
+        	datetime = datetime.substr(0, datetime.indexOf('+'));
+        	fields.push({name:'has_reply_datetime',value:datetime});
         }
       }            
       // Annotations
+      if (-1!=s.indexOf('urn:scalar:anno')) {
+        if (current_uri==data[s]['http://www.openannotation.org/ns/hasBody'][0].value) {
+        	fields.push({name:'annotation_of',value:get_urn_from_uri(data,data[s]['http://www.openannotation.org/ns/hasTarget'][0].value)});
+        	var points = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#xywh=')+6);
+        	var line_num = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#line=')+6);
+        	var start_line_num = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#line=')) ? line_num.split(',')[0] : 0;
+        	var end_line_num = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#line=')) ? line_num.split(',')[1] : 0;
+        	var seconds = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#npt=')+5);
+        	var start_seconds = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#npt=')) ? seconds.split(',')[0] : 0;
+        	var end_seconds = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#npt=')) ? seconds.split(',')[1] : 0;    
+        	fields.push({name:'annotation_of_points',value:points});    
+			fields.push({name:'annotation_of_start_line_num',value:start_line_num});
+			fields.push({name:'annotation_of_end_line_num',value:end_line_num});
+			fields.push({name:'annotation_of_start_seconds',value:start_seconds});
+			fields.push({name:'annotation_of_end_seconds',value:end_seconds});        	
+        } else {
+        	fields.push({name:'has_annotation',value:get_urn_from_uri(data,data[s]['http://www.openannotation.org/ns/hasBody'][0].value)});
+        	var points = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#xywh=')+6);
+        	var line_num = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#line=')+6);
+        	var start_line_num = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#line=')) ? line_num.split(',')[0] : 0;
+        	var end_line_num = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#line=')) ? line_num.split(',')[1] : 0;
+        	var seconds = data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.substr(data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#npt=')+5);
+        	var start_seconds = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#npt=')) ? seconds.split(',')[0] : 0;
+        	var end_seconds = (-1!=data[s]['http://www.openannotation.org/ns/hasTarget'][0].value.indexOf('#npt=')) ? seconds.split(',')[1] : 0;    
+        	fields.push({name:'has_annotation_points',value:points});    
+			fields.push({name:'has_annotation_start_line_num',value:start_line_num});
+			fields.push({name:'has_annotation_end_line_num',value:end_line_num});
+			fields.push({name:'has_annotation_start_seconds',value:start_seconds});
+			fields.push({name:'has_annotation_end_seconds',value:end_seconds});
+        }
+      }          
       // Tags
       if (-1!=s.indexOf('urn:scalar:tag')) {
         if (current_uri==data[s]['http://www.openannotation.org/ns/hasBody'][0].value) {
@@ -58,6 +97,12 @@ function insert_rel_fields(current_urn, current_slug) {
         }
       }
       // References
+      if ('undefined'!=typeof(data[s]['http://purl.org/dc/terms/isReferencedBy'])) {
+      	var version = data[s]['http://purl.org/dc/terms/isReferencedBy'][0].value;
+      	version = data[version]['http://scalar.usc.edu/2012/01/scalar-ns#version'][0].value;
+      	fields.push({name:'has_reference',value:get_urn_from_uri(data,version)});
+      	fields.push({name:'has_reference_reference_text',value:''})
+      }                
     }
     for (var j = 0; j < fields.length; j++) {
       insert_into.append('<input type="hidden" name="'+fields[j].name+'" value="'+fields[j].value+'" />');
