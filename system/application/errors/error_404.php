9d814cc73a338cb4e798b4142e7e199618f8291b
Michael Lynch <michaellynch628@gmail.com>
2014-07-11 21:36:48 -0700

    Themed 404 Page to Match Scalar Login

--- system/application/errors/error_404.php
+++ system/application/errors/error_404.php
@@ -2,33 +2,78 @@
 <head>
 <title>404 Page Not Found</title>
 <style type="text/css">
-
 body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
+	margin: 0px 0px 0px 0px;
+	font-family: 'Lucida Grande', 'Lucida Sans Unicode', sans-serif;
+	color: #000000;
+	font-size: 0.8em;
+	background-color: #ffffff;
+	background: url('<?=confirm_slash(base_url())?>/system/application/views/arbors/admin/images/background.png') no-repeat center top #f8f8f8;
 }
 
-#content  {
-border:				solid 1px #2b9ac2;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
+.panel {
+	background-color: white;
+	border: solid 1px #e7e5e0;
+	padding: 10px;
+	margin-bottom: 6px;
 }
 
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#000000;
-margin: 			0 0 4px 0;
+.login_wrapper {
+	font-family: 'Lato', Arial, sans-serif;
+	width: 300px;
+	position: absolute;
+	top: 28px;
+	left: 50%;
+	margin-left: -150px;
+	font-size: 100%;
+}
+
+.login_wrapper h4 {
+	font-size:13px;
+}
+
+.login_header {
+	vertical-align: middle;
+	padding-bottom: 10px;
+	text-align: center;
+}
+
+.login_footer {
+	font-size: 12px;
+	text-align: center;
+	padding-top: 6px;
+}
+a {
+	text-decoration: none;
+	color: #026697;
+}
+.form_fields td {
+	padding: 6px 10px 6px 10px;
 }
 </style>
 </head>
 <body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+<div class="system_wrapper">
+	<div class="content">
+		<div class="login_wrapper">
+			<table class="panel form_fields">
+				<tbody>
+					<tr>
+						<td class="login_header" colspan="2">
+							<img src="<?=confirm_slash(base_url())?>/system/application/views/modules/login/scalar_logo.png" alt="scalar_logo" width="75" height="68">
+							<h4><?php echo $heading; ?></h4>
+							<?php echo $message; ?>
+						</td>
+					</tr>
+				</tbody>
+			</table>			
+			<div class="login_footer">
+				<a href="<?=base_url()?>">Return to index</a> | 
+				<a href="http://scalar.usc.edu/terms-of-service/" target="_blank">Terms of Service</a>
+			</div>
+		</div>
+		<br clear="both">	
 	</div>
-</body>
+	<br clear="both">
+</div>
 </html>
