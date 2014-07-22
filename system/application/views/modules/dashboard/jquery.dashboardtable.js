a2277d499301ca3e2d456cb61d649de99f31001c
ANVC <alliance4nvc@gmail.com>
2014-07-21 19:54:28 -0700

    Remove unused

--- system/application/views/modules/dashboard/jquery.dashboardtable.js
+++ system/application/views/modules/dashboard/jquery.dashboardtable.js
@@ -18,22 +18,11 @@
  */  
 
 /**
- * @projectDescription  Draw table of nodes
+ * @projectDescription  Draw content tables from RDF API
  * @author              Craig Dietrich
  * @version             1.0
  * @required			Instantiated scalarapi obj (default: window['scalarapi'])
  */ 
-
-if ('undefined'==typeof(escape_html)) {
-	function escape_html(unsafe) {
-	    return unsafe
-	         .replace(/&/g, "&amp;")
-	         .replace(/</g, "&lt;")
-	         .replace(/>/g, "&gt;")
-	         .replace(/"/g, "&quot;")
-	         .replace(/'/g, "&#039;");
-	 }
-}
 
 (function($) {
 
@@ -203,71 +192,17 @@ if ('undefined'==typeof(escape_html)) {
 		basename : function(path, suffix) {
 			
 			  if (!path || !path.length) return '';  // Added by Craig
-			
 			  var b = path;
 			  var lastChar = b.charAt(b.length - 1);
-
 			  if (lastChar === '/' || lastChar === '\\') {
 			    b = b.slice(0, -1);
 			  }
-
 			  b = b.replace(/^.*[\/\\]/g, '');
-
 			  if (typeof suffix === 'string' && b.substr(b.length - suffix.length) == suffix) {
 			    b = b.substr(0, b.length - suffix.length);
 			  }
-
 			  return b;			
 			
-		},
-		
-		// http://phpjs.org/functions/htmlspecialchars/
-		htmlspecialchars : function (string, quote_style, charset, double_encode) {
-
-			  var optTemp = 0,
-			    i = 0,
-			    noquotes = false;
-			  if (typeof quote_style === 'undefined' || quote_style === null) {
-			    quote_style = 2;
-			  }
-			  string = string.toString();
-			  if (double_encode !== false) { // Put this first to avoid double-encoding
-			    string = string.replace(/&/g, '&amp;');
-			  }
-			  string = string.replace(/</g, '&lt;')
-			    .replace(/>/g, '&gt;');
-
-			  var OPTS = {
-			    'ENT_NOQUOTES': 0,
-			    'ENT_HTML_QUOTE_SINGLE': 1,
-			    'ENT_HTML_QUOTE_DOUBLE': 2,
-			    'ENT_COMPAT': 2,
-			    'ENT_QUOTES': 3,
-			    'ENT_IGNORE': 4
-			  };
-			  if (quote_style === 0) {
-			    noquotes = true;
-			  }
-			  if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
-			    quote_style = [].concat(quote_style);
-			    for (i = 0; i < quote_style.length; i++) {
-			      // Resolve string input to bitwise e.g. 'ENT_IGNORE' becomes 4
-			      if (OPTS[quote_style[i]] === 0) {
-			        noquotes = true;
-			      } else if (OPTS[quote_style[i]]) {
-			        optTemp = optTemp | OPTS[quote_style[i]];
-			      }
-			    }
-			    quote_style = optTemp;
-			  }
-			  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
-			    string = string.replace(/'/g, '&#039;');
-			  }
-			  if (!noquotes) {
-			    string = string.replace(/"/g, '&quot;');
-			  }
-
-			  return string;
 		}
 		
 	};
