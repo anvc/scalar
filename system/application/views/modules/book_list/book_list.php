5e4d1f54c40d011793479f3296f26cfceaa437e2
Craig Dietrich <craigdietrich@gmail.com>
2014-07-24 11:00:40 -0700

    Book list: alert if no sq

--- system/application/views/modules/book_list/book_list.php
+++ system/application/views/modules/book_list/book_list.php
@@ -57,7 +57,7 @@ if (count($featured_books) > 0) {
 }
 ?>
 <h3><?=lang('welcome.other_books')?></h3>
-<form action="<?=base_url()?>" id="book_list_search">
+<form action="<?=base_url()?>" id="book_list_search" onsubmit="if (!this.sq.value.length) {alert('Please enter a search term');return false;}">
 <div>
 <div><input type="text" name="sq" class="generic_text_input" /></div>
 <div><input type="submit" class="generic_button" value="Search" /></div>
