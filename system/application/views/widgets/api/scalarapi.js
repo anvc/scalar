fa6e67ef1a061f6ccef9bb178a8f0e4d57f3a69b
Erik Loyer <erik@song.nu>
2014-08-12 09:39:20 -0700

    Add type option to nodeSearch

--- system/application/views/widgets/api/scalarapi.js
+++ system/application/views/widgets/api/scalarapi.js
@@ -2158,6 +2158,8 @@ ScalarAPI.prototype.loadNodesByType = ScalarAPI.prototype.loadPagesByType = func
  * @param	relation			If true, will return only relations of the named type
  * @param	start				Result number to start with
  * @param	results				Number of results to return
+ * @param	hidden				Include results where live is set to 0
+ * @param	type				Only search in specific content types
  * @return						A string indicating the state of the request.
  */
 ScalarAPI.prototype.nodeSearch = function(sq, successCallback, errorCallback, depth, references, relation, start, results, hidden) {
@@ -2186,9 +2188,13 @@ ScalarAPI.prototype.nodeSearch = function(sq, successCallback, errorCallback, de
 		queryString += '&hidden='+hidden;
 	}
 	
+	if ( type == null ) {
+		type = 'content';
+	}
+	
 	$.ajax({
 		type:"GET",
-		url:this.model.urlPrefix+'rdf/instancesof/content?'+queryString,
+		url:this.model.urlPrefix+'rdf/instancesof/' + type + '?'+queryString,
 		dataType:"jsonp",
 		success:[this.parsePagesByType, successCallback],
 		error:[this.handleLoadPagesByTypeError, errorCallback]
