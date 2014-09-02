4393474962dd97ff397c032f196618b6d1295636
Craig Dietrich <craigdietrich@gmail.com>
2014-09-01 21:44:11 -0700

    Tinypass temp landing page style

--- system/application/views/melons/cantaloupe/tinypass.php
+++ system/application/views/melons/cantaloupe/tinypass.php
@@ -2,12 +2,13 @@
 <style>
 body {font-family:Helvetica; margin:100px;}
 </style>
-<h3>Scalar Temporary Tinypass Landing Page (Cantaloupe)</h3>
-<h4><?=$page->versions[$page->version_index]->title?> is part of this <?=$book->scope?>'s premium content</h4>
+<h3><?=$book->title?></h3>
+<h4>"<?=$page->versions[$page->version_index]->title?>" is part of this <?=$book->scope?>'s premium content</h4>
 <p>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in ultricies orci. Ut quis sapien vestibulum, fringilla ligula a, facilisis tortor. Sed sagittis neque et arcu molestie cursus. Praesent quis libero eu ipsum pellentesque tincidunt. Nullam pulvinar turpis id consequat fringilla. Curabitur ut fermentum elit. Vestibulum eu urna egestas, ullamcorper odio congue, pharetra justo. Sed sit amet felis nisl. Etiam eget egestas tellus, ac tempus tortor. Duis ornare pretium tellus nec condimentum.
 </p>
 <p>
-Donec elit tellus, adipiscing vitae mi a, commodo adipiscing leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nam auctor eget nunc id dictum. Vestibulum in pulvinar ante, cursus consequat turpis. Integer porta gravida elit, commodo placerat arcu bibendum non. Aliquam aliquam posuere lectus at iaculis. Etiam vel accumsan nisl. In semper venenatis quam et tincidunt. Quisque molestie nec velit a dictum.
+Tinypass Cantaloupe landing page
 </p>
+<br />
 <div id="tinypass-button"><?=$buttonHTML?></div>
