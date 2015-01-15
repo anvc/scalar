fcd7bf6c3d0dea936a79b8bde9c9652854c28bb0
Craig Dietrich <craigdietrich@gmail.com>
2015-01-14 17:08:27 -0800

    Tighten up isURL()

--- system/application/helpers/MY_url_helper.php
+++ system/application/helpers/MY_url_helper.php
@@ -75,7 +75,7 @@
     // TODO: use parse_url() [http://stackoverflow.com/questions/6240414/add-http-prefix-to-url-when-missing]
     function isURL($uri='') {
 
-    	return (strstr($uri, '://') || substr($uri, 0, 4)=='urn:') ? true : false;
+    	return (substr($uri,0,7)== 'http://'||substr($uri,0,8)== 'https://'||substr($uri,0,4)=='urn:') ? true : false;
 
     }
     
