ebd755dfc4b646e66be8bbf6536d5b7cf4463281
Craig Dietrich <craigdietrich@gmail.com>
2015-03-24 18:08:41 -0700

    Update text for Honeydew warnings for default views

--- system/application/views/melons/honeydew/content/edit.js
+++ system/application/views/melons/honeydew/content/edit.js
@@ -284,10 +284,9 @@ function validate_form($form, ignoreViewCheck) {
 		// If default view is 'plain', send warning if media has been linked in the WYSIWYG
 		var default_view_value = $("[name='scalar:default_view'] option:selected").val();
 		var default_view_name = $("[name='scalar:default_view'] option:selected").html();
-		console.log(default_view_value);
 		if ('plain'==default_view_value) {
 			var textarea_content = $('#edit_content textarea:first').val();
-			var confirm_default_view_msg = 'The page content appears to have one or more links to imported Scalar media.  However, the default view of the page is set to empty.  Pages with embedded media work best in other views such as text-emphasis or media-emphasis.  Do you wish to save this page with the current settings?';
+			var confirm_default_view_msg = 'The page content appears to have one or more links to imported Scalar media.  However, the layout of the page is set to its default value.  Pages with embedded media work best in other layouts such as text-emphasis or media-emphasis.  Do you wish to save this page with the current settings?';
 			try {
 				$('<div>'+textarea_content+'</div>').find('a:not(.inline)').each(function() {
 					if ($(this).attr('resource')) throw 'resource link present';
@@ -298,9 +297,9 @@ function validate_form($form, ignoreViewCheck) {
 		}
 	
 		// If no media has been linked, send warning (ie, media-emphasis view)
-		var media_views = ['text','media','split','par'];
+		var media_views = ['text','media','split','par','revpar'];
 		if (media_views.indexOf(default_view_value)!=-1) {
-			var confirm_default_view_msg = 'You have selected a default view, '+default_view_name+', that works best when media have been connected directly to pieces of text (e.g., using the first blue button in the text editor). However, it appears there are no direct text-media links established.  We recommend changing the default view to empty or establishing direct text-media links.  Do you wish to save this page with the current settings?';
+			var confirm_default_view_msg = 'You have selected a layout, '+default_view_name+', that works best when media have been connected directly to pieces of text (e.g., using the first blue button in the text editor). However, it appears there are no direct text-media links established.  We recommend changing the layout to empty or establishing direct text-media links.  Do you wish to save this page with the current settings?';
 			var textarea_content = $('#edit_content textarea:first').val();
 			if (!$('<div>'+textarea_content+'</div>').find('a:not(.inline)').length) {
 				if (!confirm(confirm_default_view_msg)) return false;
