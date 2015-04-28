2b9cde84eb98ffb6edbe11a1421bb0f95448a0ca
Erik Loyer <erikcloyer@gmail.com>
2015-04-28 15:35:07 -0700

    Restore dropped continueToContentId property

--- system/application/views/widgets/api/scalarapi.js
+++ system/application/views/widgets/api/scalarapi.js
@@ -2332,6 +2332,7 @@ function ScalarModel(options) {
 		{property:'urn', uri:'http://scalar.usc.edu/2012/01/scalar-ns#urn', type:'string'},
 		{property:'sortNumber', uri:'http://scalar.usc.edu/2012/01/scalar-ns#sort_number', type:'int'},
 		{property:'continueTo', uri:'http://scalar.usc.edu/2012/01/scalar-ns#continue_to', type:'int'},
+		{property:'continueToContentId', uri:'http://scalar.usc.edu/2012/01/scalar-ns#continue_to_content_id', type:'int'},
 		{property:'isLive', uri:'http://scalar.usc.edu/2012/01/scalar-ns#isLive', type:'int'},
 		{property:'paywall', uri:'http://scalar.usc.edu/2012/01/scalar-ns#paywall', type:'string'},
 		{property:'fullname', uri:'http://scalar.usc.edu/2012/01/scalar-ns#fullname', type:'string'},
