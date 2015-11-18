8033617d6e3ccc280323214d424a2f0ad596dfd3
Craig Dietrich <craigdietrich@gmail.com>
2015-11-18 11:18:11 -0800

    Garbage collect

--- system/application/plugins/tensor/application/models/handlers/vimeo.php
+++ system/application/plugins/tensor/application/models/handlers/vimeo.php
@@ -1,6 +1,6 @@
 <?php
-$client_id = '6c51c66e8d70204578142f8048abc9204050d54c';
-$client_secret = 'EmdMMY4bLRXpm4bNyx/Mt9i6Vt/N3DtRY36wRif3i57xjdPCsDKRf/tYuckIr+qFEk2aNNKC/6NFKbEW9Q8jUDzDK2yY8yO/+hSCZftHq5tHC+vkikT5NwXZC8j0vtMt';
+$client_id = '';
+$client_secret = '';
 
 require(FCPATH."application/models/handlers/vimeo/autoload.php");
 $lib = new \Vimeo\Vimeo($client_id, $client_secret);
