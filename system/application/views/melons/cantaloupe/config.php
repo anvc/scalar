7cb8530646ab4538c87e58f63938bd8712c28403
Lucas Miller <lucas@rustltd.com>
2016-03-31 19:05:06 -0700

    Added ability to select one/multiple/all or none of the annotations on an embedded media element to be displayed via ckeditor

--- system/application/views/melons/cantaloupe/config.php
+++ system/application/views/melons/cantaloupe/config.php
@@ -65,12 +65,14 @@ $config['reference_options'] = array(
 	'insertMediaLink' 		=> array(
 								'size' => array('small', 'medium', 'large', 'native', 'full'),
 								'align' => array('right', 'left'),
-								'caption' => array('description', 'title', 'title-and-description', 'none')
+								'caption' => array('description', 'title', 'title-and-description', 'none'),
+								'annotations' => array('none','all','list')
 						 		),
 	'insertMediaelement' 	=> array(
 								'size' => array('small', 'medium', 'large', 'native', 'full'),
 								'align' => array('left', 'center', 'right'),
-								'caption' => array('description', 'title', 'title-and-description', 'none')
+								'caption' => array('description', 'title', 'title-and-description', 'none'),
+								'annotations' => array('none','all','list')
 						    	),
 	'insertAnnotation' 		=> array(
 								'size' => array('small', 'medium', 'large', 'native', 'full'),
