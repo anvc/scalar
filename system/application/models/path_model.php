7ba1c9ca7866eab5eae2305cd8e0c51a1376f8ab
Craig Dietrich <craigdietrich@gmail.com>
2016-06-26 11:16:56 -0700

    Allow paths that contain the same page twice to be properly expressed in RDF

--- system/application/models/path_model.php
+++ system/application/models/path_model.php
@@ -26,7 +26,7 @@
 
 class Path_model extends MY_Model {
 
-	private $urn_template = 'urn:scalar:path:$1:$2';
+	private $urn_template = 'urn:scalar:path:$1:$2:$3';
 
     public function __construct() {
 
@@ -34,10 +34,14 @@ class Path_model extends MY_Model {
 
     }
 
-	public function urn($pk_1=0, $pk_2=0) {
+	public function urn($pk_1=0, $pk_2=0, $fields=null) {
+
+		$cardinal = 0;
+		if (is_object($fields) && isset($fields->sort_number)) $cardinal = (int) $fields->sort_number;
 
 		$return = str_replace('$1', $pk_1, $this->urn_template);
 		$return = str_replace('$2', $pk_2, $return);
+		$return = str_replace('$3', $cardinal, $return);
 		return $return;
 
 	}
