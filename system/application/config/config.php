c7b7f02cae50aae5b175dfef8c4519bbde52e824
Craig Dietrich <craigdietrich@gmail.com>
2016-07-24 17:34:46 -0700

    Users experiencing logouts on some servers

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -258,15 +258,15 @@ $config['encryption_key'] = (getenv('SCALAR_ENCRYPTION_KEY') ? getenv('SCALAR_EN
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
-$config['sess_encrypt_cookie']	        = TRUE;
-$config['sess_use_database']	        = TRUE;
-$config['sess_table_name']		= 'scalar_db_sessions';
-$config['sess_match_ip']		= FALSE;
-$config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	        = 300;
+$config['sess_cookie_name']     = 'ci_session';
+$config['sess_expiration']      = 14400;
+$config['sess_expire_on_close'] = FALSE;
+$config['sess_encrypt_cookie']  = TRUE;
+$config['sess_use_database']    = TRUE;
+$config['sess_table_name']      = 'scalar_db_sessions';
+$config['sess_match_ip']        = FALSE;
+$config['sess_match_useragent'] = TRUE;
+$config['sess_time_to_update']  = 300;
 
 /*
 |--------------------------------------------------------------------------
