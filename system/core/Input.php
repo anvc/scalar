f39e5d7e8e88ceba778dd30a514ccd43fe3d8b98
Craig Dietrich <craigdietrich@gmail.com>
2016-12-08 14:57:47 -0800

    Remove hanging echo

--- system/core/Input.php
+++ system/core/Input.php
@@ -745,7 +745,6 @@ class CI_Input {
 	function _clean_input_keys($str)
 	{
 	 /* if ( ! preg_match("/^[a-z0-9:_\/-]+$/i", $str)) */
-		echo $str."\n";
 		if ( ! preg_match("/^[a-z0-9:?,\{\}:\"=_\/\-~@]+$/i", $str))
 		{
 			exit('Disallowed Key Characters.');
