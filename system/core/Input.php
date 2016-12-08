f14eef15ef88d212056f6a8fee66bc672af9799d
Craig Dietrich <craigdietrich@gmail.com>
2016-12-08 14:35:38 -0800

    Setup Critical Commons upload system

--- system/core/Input.php
+++ system/core/Input.php
@@ -745,9 +745,9 @@ class CI_Input {
 	function _clean_input_keys($str)
 	{
 	 /* if ( ! preg_match("/^[a-z0-9:_\/-]+$/i", $str)) */
-		if ( ! preg_match("/^[a-z0-9:?,=_\/\-~@]+$/i", $str))
+		if ( ! preg_match("/^[a-z0-9:?,=\[\]\{\}%'_\/\-~@]+$/i", $str))
 		{
-			exit('Disallowed Key Characters.');
+			//exit('Disallowed Key Characters.');
 		}
 
 		// Clean UTF-8 if supported
