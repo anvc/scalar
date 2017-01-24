197fccc1b6bedf5e09dc0236be67f0612c055246
Craig Dietrich <craigdietrich@gmail.com>
2017-01-24 13:42:55 -0800

    Add Scalar version number to views directory

---
+++ system/application/views/scalar-version.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * This version number represents the last Scalar Release on Github:
+ * https://github.com/anvc/scalar/releases
+ * 
+ * Further commits might have been made to the repository since then;
+ * the number is only updated just before a new Release is published.
+ * 
+ * You can write the HTML below into any view file, for example:
+ * echo '<p>';
+ * echo 'Scalar version number: ';
+ * $this->load->view('scalar-version',true);
+ * echo '</p>';
+ */
+?>
+2.1.3
