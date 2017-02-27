004153ec79b5ce9d15f441f033cecd7b3bfc60aa
Erik Loyer <erikcloyer@gmail.com>
2017-02-27 10:14:02 -0800

    Allow any click to dismiss context popover

--- system/application/views/melons/cantaloupe/js/scalarpage.jquery.js
+++ system/application/views/melons/cantaloupe/js/scalarpage.jquery.js
@@ -525,6 +525,19 @@
                 }
             },
 
+            allowAnyClickToDismissPopovers: function() {
+                $(document).on('click', function (e) {
+                    $('[data-toggle="popover"],[data-original-title]').each(function () {
+                        //the 'is' for buttons that trigger popups
+                        //the 'has' for icons within a button that triggers a popup
+                        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
+                            (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
+                        }
+
+                    });
+                });
+            },
+
             addHeaderPathInfo: function() {
 
                 // show containing path in header
@@ -2706,6 +2719,7 @@
                     page.addColophon();
                     if (viewType != 'edit') {
                         page.addContext();
+                        page.allowAnyClickToDismissPopovers();
                     }
                     break;
 
