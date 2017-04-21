a4736b8ef9e9cf4df2d33a5e1cc256f2accf0bf0
Christopher R. Maden <crism@maden.org>
2017-04-21 01:37:07 -0500

    anvc/scalar#62 interim checkin

--- system/application/rdf/omeka_s.php
+++ system/application/rdf/omeka_s.php
@@ -47,12 +47,6 @@ if (empty($page)) $page = 1;
 
 if (substr($uri, -1, 1) == '/') $uri = substr($uri, 0, -1);
 
-$files_url = $uri.'/api/items';
-$files =@ file_get_contents($files_url);
-if (!$files) die('{}');
-$files = json_decode($files);
-if (!is_array($files)) die('{}');
-
 if (1==$page) {
 	$items_url = 'http://'.$uri.'/api/items?page='.$page;
 	$items =@ file_get_contents($items_url);
@@ -98,55 +92,6 @@ $locations = s_array_merge($s_locations, $locations);
 $_SESSION[urlencode('http://'.$uri.'/api/geolocations')] = $locations;
 
 $output = array();
-
-foreach ($files as $file) {
-	$obj = new stdClass();
-	$obj->id = $file->id;
-	$obj->url = $file->file_urls->original;
-	$obj->thumb = $file->file_urls->thumbnail;
-	$obj->filename = $file->original_filename;
-	$obj->archive = $uri;
-	$obj->added = $file->added;
-	$obj->element_texts = new stdClass();
-	// First try to get Dublin Core data from the file
-	foreach ($file->element_texts as $text) {
-		if ($text->element_set->name != 'Dublin Core') continue;
-		$name = $text->element->name;
-		$obj->element_texts->$name = trim(str_replace("\r",'',str_replace("\n",'',nl2br($text->text))));
-	}
-	// See what Dublin Core is available from the parent item
-	$item_id = $file->item->id;
-	for ($j = 0; $j < count($items); $j++) {
-		if ($items[$j]->id != $item_id) continue;
-		$obj->owner = $items[$j]->owner;
-		$obj->tags = $items[$j]->tags;
-		// DC Terms
-		foreach ($items[$j]->element_texts as $text) {
-			if ($text->element_set->name != 'Dublin Core') continue;
-			$name = $text->element->name;
-			if (!isset($obj->element_texts->$name)) {
-				$obj->element_texts->$name = trim(str_replace("\r",'',str_replace("\n",'',nl2br($text->text))));
-			}
-		}
-		// Geolocations
-		$location = null;
-		if (isset($items[$j]->extended_resources->geolocations->id)) {
-			$geo_id = $items[$j]->extended_resources->geolocations->id;
-			for ($k = 0; $k < count($locations); $k++) {
-				if ($locations[$k]->id == $geo_id) $location = $locations[$k]->latitude.','.$locations[$k]->longitude;
-			}
-		}
-		$obj->location_data = $location;
-	}
-	if (!isset($obj->element_texts->Title) && isset($obj->filename) && strpos($obj->filename,'.')) {
-		$obj->element_texts->Title = spacify(substr($obj->filename, 0, strpos($obj->filename,'.')));
-	} elseif (!isset($obj->element_texts->Title) && isset($obj->filename) && strpos($obj->filename,'.')) {
-		$obj->element_texts->Title = $obj->filename;
-	} elseif (!isset($obj->element_texts->Title)) {
-		$obj->element_texts->Title = 'Unknown Title';
-	}
-	$output[] = $obj;
-}
 
 header('Content-Type: application/json');
 echo json_encode($output);
