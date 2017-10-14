9f198e50a9ef7ad3ac8d5804ee041c1e1c2b9d21
Craig Dietrich <craigdietrich@gmail.com>
2017-10-13 17:44:54 -0700

    User page template style adjustments

--- system/application/views/melons/cantaloupe/users.php
+++ system/application/views/melons/cantaloupe/users.php
@@ -1,7 +1,14 @@
 <?php if (empty($book_user)): ?>
 <div id="centered-message"><span>This is the user page template, however no user with the asked-for ID exists in this <?=$book->scope?>.</span></div>
 <?php elseif (!isset($page) || empty($page)): ?>
-<div id="centered-message"><span>This is a user page for <?=$book_user->fullname?>, but it hasn't been created yet.</span> <span>Click the <img src="http://localhost/scalar/system/application/views/melons/cantaloupe/images/edit_icon.png" alt="Edit button. Click to edit the current page or media." width="30" height="30"> button above to create one.</span></div>
+<div id="centered-message"><span>This is a user page for <?php
+if (!empty($book_user->url)) echo '<a href="'.$book_user->url.'">';
+echo $book_user->fullname;
+if (!empty($book_user->url)) echo '</a>';
+?>, but it hasn't been created yet.</span>
+<?php if ($login->is_logged_in): ?> 
+<span>Click the <img src="<?=base_url()?>system/application/views/melons/cantaloupe/images/edit_icon.png" alt="Edit button. Click to edit the current page or media." width="30" height="30"> button above to create one.</span></div>
+<?php endif; ?>
 <?php endif; ?>
 <?php
 if (!empty($book_user->url)) {
