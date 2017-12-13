4f6aebde2b2e0fe9d810793a5fca0ee027dbc44d
Erik Loyer <erik@song.nu>
2017-12-13 10:34:04 -0800

    Set mobile viewport for dashboot

--- system/application/views/modules/dashboot/content.php
+++ system/application/views/modules/dashboot/content.php
@@ -1,4 +1,5 @@
 <?if (!defined('BASEPATH')) exit('No direct script access allowed')?>
+<?$this->template->add_meta('viewport','width=device-width, initial-scale=1');?>
 <?$this->template->add_css('system/application/views/modules/dashboot/css/bootstrap.min.css')?>
 <?$this->template->add_css('system/application/views/modules/dashboot/css/tabs.css')?>
 <?$this->template->add_js('system/application/views/modules/dashboot/js/jquery-3.1.0.min.js')?>
