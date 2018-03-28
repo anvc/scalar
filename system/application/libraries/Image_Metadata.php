b4b5fc961054d87950dbfc26848b7bc96c2d4712
Craig Dietrich <craigdietrich@gmail.com>
2018-03-28 13:36:47 -0700

    Remove exif_read_data filetype warnings

--- system/application/libraries/Image_Metadata.php
+++ system/application/libraries/Image_Metadata.php
@@ -91,7 +91,8 @@ class Image_Metadata {
 
 	private function get_exif($path='') {
 
-		$exif =@ exif_read_data($path);
+		$exif = array();
+		if (exif_imagetype($path)) $exif = exif_read_data($path);
 		
 		if (isset($exif['GPSLongitude']) && isset($exif['GPSLongitudeRef']) && isset($exif['GPSLatitude']) && isset($exif['GPSLatitudeRef'])) {
 			if (is_array($exif['GPSLongitude']) && is_array($exif['GPSLatitude'])) {
