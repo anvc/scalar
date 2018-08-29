6fdd2936b96db02ac604fd0a372dd5fe6750ca20
Erik Loyer <erik@song.nu>
2018-08-28 17:01:59 -0700

    Reposition dashboard revert option

--- system/application/views/modules/dashboot/content.php
+++ system/application/views/modules/dashboot/content.php
@@ -35,10 +35,6 @@ $(window).on('scroll',function() {
 });
 </script>
 
-<div style="position:absolute; top:0px; left:0px; width:100%; color:#444444; font-size:1.25rem;  text-align:center; padding-top:4px;">
-	<a href="?dashboard=dashboard&book_id=<?=((!empty($book))?$book->book_id:0)?>&zone=style#tabs-style">Revert to the classic Dashboard</a>
-</div>
-
 <div class="bgcolor">
 
 <header class="container-fluid">
@@ -70,6 +66,7 @@ $(window).on('scroll',function() {
     <nav class="col-xs-12 col-sm-6">
       <h5>
         <a href="http://scalar.usc.edu">About Scalar</a> | 
+        <a href="?dashboard=dashboard&book_id=<?=((!empty($book))?$book->book_id:0)?>&zone=style#tabs-style">Revert Dashboard</a> | 
         <a href="http://scalar.usc.edu/works/guide2">User's Guide</a> 
         <img class="scalar_logo small" src="<?=$app_root.'views/modules/dashboot/images/scalar-avatar.png'?>" alt="Small Scalar logo" />
       </h5>
