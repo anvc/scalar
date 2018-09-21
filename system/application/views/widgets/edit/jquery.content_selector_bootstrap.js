cc4fda4a63ad4b354de6974ee5d0ccecfc0ed7fa
Lucas <lucas@rustltd.com>
2018-09-21 14:19:41 -0400

    Fix content selector height overflowing on Safari

--- system/application/views/widgets/edit/jquery.content_selector_bootstrap.js
+++ system/application/views/widgets/edit/jquery.content_selector_bootstrap.js
@@ -1781,7 +1781,7 @@ isMac = navigator.userAgent.indexOf('Mac OS X') != -1;
 			height -= (heading_height+body_height+footer_height);
 			height = Math.max(height, 300);
 
-			$dialogue_container.find('.node_selector_table_body').height(height).find('table').show();
+			$dialogue_container.find('.node_selector_table_body').css('min-height',height+'px').find('table').show();
 
 			var body = $dialogue_container.find('.node_selector_table_body')[0];
 
