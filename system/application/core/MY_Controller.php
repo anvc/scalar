88234147bd4a2a1334236f882c0faa1fab19fa8d
Craig Dietrich <craigdietrich@gmail.com>
2019-01-21 11:39:39 -0800

    Editorial Workflow: disallow Honeydew books

--- system/application/core/MY_Controller.php
+++ system/application/core/MY_Controller.php
@@ -321,6 +321,7 @@ class MY_Controller extends CI_Controller {
 		
 		$can = true;
 		if (!isset($this->data['book']) || !isset($this->data['book']->editorial_is_on)) $can = false;
+		if ('honeydew' == $this->data['book']->template) $can = false;
 		if (!$this->db->field_exists('editorial_state', 'versions')) $can = false;
 		if (!$this->db->field_exists('usage_rights', 'versions')) $can = false;
 		if (!$this->db->field_exists('editorial_queries', 'versions')) $can = false;
