ef2c3e0724b52c69c876695beaacd803491d68b0
Craig Dietrich <craigdietrich@gmail.com>
2019-04-21 17:20:37 -0700

    Add ability to set default tab on the edit page

--- system/application/views/melons/cantaloupe/edit.php
+++ system/application/views/melons/cantaloupe/edit.php
@@ -36,6 +36,9 @@ $this->template->add_js('var media_views='.json_encode( $this->config->item('med
 if ($this->config->item('predefined_css')) {
 	$this->template->add_js('var predefined_css='.json_encode($this->config->item('predefined_css')), 'embed');
 }
+if (!empty($default_tabs)) {
+	$this->template->add_js('var default_tabs='.json_encode($default_tabs), 'embed');
+}
 $css = <<<END
 .cke_button.cke_button__removeformat{border: 1px solid rgba(200,50,50,.3); background-color: rgba(200,50,50,.2); box-shadow: 0 0 10px rgba(255,0,0,.2) inset;border: none;}
 .cke_button.cke_button__removeformat:hover{box-shadow: none;}
@@ -341,7 +344,6 @@ $(document).ready(function() {
 		    });
 		}
 	});
-
 	// Thumbnail
 	var choose_thumb = $('#choose_thumbnail');
 	var thumbnail = $('input[name="scalar:thumbnail"]');
@@ -463,7 +465,6 @@ $(document).ready(function() {
 			});
 		});
 	};
-
 	//Added to prevent accidental navigation away from edit/add page - matches all anchor tags
 	//with an href attribute that doesn't start with # or javascript:
 	$(document).on('click', 'a[href]:not([href=""], [href^="#"], [href^="javascript"], [target="_blank"])', function(e){
@@ -472,11 +473,17 @@ $(document).ready(function() {
 			return false;
 		}
 	});
-
-	//Editorial state stuff:
-
+	// Default tabs
+	if ('undefined' != typeof(default_tabs)) {
+		var selected =  $("input:radio[name='rdf:type']:checked").val();
+		if (selected.indexOf('Composite')!=-1 && 'undefined'!=typeof(default_tabs['composite'])) {
+			if ($('a[href="'+default_tabs['composite']+'"]').length) $('a[href="'+default_tabs['composite']+'"]').click();
+        } else if (selected.indexOf('Media')!=-1 && 'undefined'!=typeof(default_tabs['media'])) {
+			if ($('a[href="'+default_tabs['media']+'"]').length) $('a[href="'+default_tabs['media']+'"]').click();
+        }
+    };
+	// Editorial state:
 	has_edition = false;
-
 	if($('a.metadata[rel="scalar:hasEdition"]').length > 0){
 		if(version_date != null){
 			var edition_timestamp = new Date($('span[resource="'+$('a.metadata[rel="scalar:hasEdition"]').attr('href')+'"] span[property="dcterms:created"]').text());
