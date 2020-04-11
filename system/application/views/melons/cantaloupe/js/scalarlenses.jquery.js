ca7bfd69be8e086acb3b173ff3e43d7dc00269cb
Michael Morgan <michaelmorgan@michaels-mbp.gateway.sonic.net>
2020-04-11 14:14:48 -0700

    JSON values now update content button text

--- system/application/views/melons/cantaloupe/js/scalarlenses.jquery.js
+++ system/application/views/melons/cantaloupe/js/scalarlenses.jquery.js
@@ -64,114 +64,121 @@
             '<div id="lens">'+
               '<div class="col-xs-12">'+
                 '<div class="lens-expand-container" data-toggle="collapse" data-target="">'+
-                  '<div class="lens-icon-wrapper col-xs-1"><span class="lens-icon"></span></div>'+
-                    '<div class="lens-content col-xs-11">'+
-                      '<h3 class="lens-title heading_font heading_weight"> Tokyo Area Commercial Networks <span class="badge">4</span></h3>' +
-                    '<div class="lens-tags">'+
-                      //
-                      /// Visualization dropdown
-                      //
-                      '<div id="visualization-btn" class="btn-group"><button id="visualization-button" type="button" class="btn btn-primary btn-xs dropdown-toggle caption_font" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-                        'Select visualization<span class="caret"></span></button>'+
-                        '<ul id="visualization-dropdown" class="dropdown-menu">'+
-                          '<li><a><span class="dropdown-item-icon force"></span>Force-Directed</a></li>'+
-                          '<li><a><span class="dropdown-item-icon grid"></span>Grid</a></li>'+
-                          '<li><a><span class="dropdown-item-icon list"></span>List</a></li>'+
-                          '<li><a><span class="dropdown-item-icon map"></span>Map</a></li>'+
-                          '<li><a><span class="dropdown-item-icon radial"></span>Radial</a></li>'+
-                          '<li><a><span class="dropdown-item-icon tree"></span>Tree</a></li>'+
-                          '<li><a><span class="dropdown-item-icon word-cloud"></span>Word Cloud</a></li>'+
-                        '</ul>'+
-                      '</div>'+
-                      //
-                      /// Content selector dropdown
-                      //
-                      '<div class="btn-group"><button id="content-selector-button" type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-                          'Select items...<span class="caret"></span></button>'+
-                        '<ul class="dropdown-menu">'+
-                          '<li class="content-selector"><a>Specific items</a></li>'+
-                          '<li><a data-toggle="modal" data-target="#modalByType">Items by type</a></li>'+
-                          '<li><a data-toggle="modal" data-target="#modalByDistance">Items by distance</a></li>'+
-                        '</ul>'+
-                      '</div>'+
-                  '</div></div>'+
-                '</div></div></div>'+
-              '</div></div></div>'+ // paragraph wrapper
+                  '<div class="lens-icon-wrapper col-xs-1">'+
+                    '<span class="lens-icon"></span>'+
+                  '</div>'+
+                  '<div class="lens-content col-xs-11">'+
+                    '<h3 class="lens-title heading_font heading_weight"> Tokyo Area Commercial Networks <span class="badge">0</span></h3>' +
+                  '<div class="lens-tags">'+
+                  //
+                  /// Visualization dropdown
+                  //
+                  '<div id="visualization-btn" class="btn-group"><button id="visualization-button" type="button" class="btn btn-primary btn-xs dropdown-toggle caption_font" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+                    'Select visualization<span class="caret"></span></button>'+
+                    '<ul id="visualization-dropdown" class="dropdown-menu">'+
+                      '<li><a><span class="dropdown-item-icon force"></span>Force-Directed</a></li>'+
+                      '<li><a><span class="dropdown-item-icon grid"></span>Grid</a></li>'+
+                      '<li><a><span class="dropdown-item-icon list"></span>List</a></li>'+
+                      '<li><a><span class="dropdown-item-icon map"></span>Map</a></li>'+
+                      '<li><a><span class="dropdown-item-icon radial"></span>Radial</a></li>'+
+                      '<li><a><span class="dropdown-item-icon tree"></span>Tree</a></li>'+
+                      '<li><a><span class="dropdown-item-icon word-cloud"></span>Word Cloud</a></li>'+
+                    '</ul>'+
+                  '</div>'+
+                  //
+                  /// Content selector dropdown
+                  //
+                  '<div class="btn-group"><button id="content-selector-button" type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+                      'Select items...<span class="caret"></span></button>'+
+                    '<ul class="dropdown-menu">'+
+                      '<li class="content-selector"><a>Specific items</a></li>'+
+                      '<li><a data-toggle="modal" data-target="#modalByType">Items by type</a></li>'+
+                      '<li><a data-toggle="modal" data-target="#modalByDistance">Items by distance</a></li>'+
+                    '</ul>'+
+                  '</div>'+
 
-              //
-              /// Modal for items by Type
-              //
-              '<div id="modalByType" class="modal fade caption_font" role="dialog">'+
-                '<div class="modal-dialog">'+
-                  '<div class="modal-content">'+
-                    '<div class="modal-body">'+
-                      //'<button type="button" class="close" data-dismiss="modal">&times;</button>'+
-                      '<p>Select all items of this type:</p>'+
-                      '<div class="btn-group"><button id="byType" type="button" class="btn btn-default btn-md dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-                          'Select item...<span class="caret"></span></button>'+
-                        '<ul class="dropdown-menu">'+
-                          '<li><a>Annotation</a></li>'+
-                          '<li><a>Comment</a></li>'+
-                          '<li><a>Media</a></li>'+
-                          '<li><a>Page</a></li>'+
-                          '<li><a>Path</a></li>'+
-                          '<li><a>Tag</a></li>'+
-                        '</ul>'+
-                      '</div>'+
-                    '</div>'+ // modal body
-                    '<div class="modal-footer">'+
-                      '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>'+
-                      '<button id="typeDone" type="button" class="btn btn-primary" data-dismiss="modal">Done</button>'+
-                  '  </div>'+
-                  '</div>'+ // modal content
-                '</div>'+ // modal dialog
+                '</div>'+ //lens-expanded container
+              '</div>'+ // col-12 wrapper
+            '</div>'+ // lens wrapper
+          '</div>'+ // row
+        '</div>'+ // body copy
+      '</div>'+ // paragraph wrapper
+
+
+      //
+      /// Modal for items by Type
+      //
+      '<div id="modalByType" class="modal fade caption_font" role="dialog">'+
+        '<div class="modal-dialog">'+
+          '<div class="modal-content">'+
+            '<div class="modal-body">'+
+              //'<button type="button" class="close" data-dismiss="modal">&times;</button>'+
+              '<p>Select all items of this type:</p>'+
+              '<div class="btn-group"><button id="byType" type="button" class="btn btn-default btn-md dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+                  'Select item...<span class="caret"></span></button>'+
+                '<ul class="dropdown-menu">'+
+                  '<li><a>Annotation</a></li>'+
+                  '<li><a>Comment</a></li>'+
+                  '<li><a>Media</a></li>'+
+                  '<li><a>Page</a></li>'+
+                  '<li><a>Path</a></li>'+
+                  '<li><a>Tag</a></li>'+
+                '</ul>'+
               '</div>'+
+            '</div>'+ // modal body
+            '<div class="modal-footer">'+
+              '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>'+
+              '<button id="typeDone" type="button" class="btn btn-primary" data-dismiss="modal">Done</button>'+
+          '  </div>'+
+          '</div>'+ // modal content
+        '</div>'+ // modal dialog
+      '</div>'+
 
-              //
-              // Modal for items by Distance
-              //
-              '<div id="modalByDistance" class="modal fade caption_font" role="dialog">'+
-                '<div class="modal-dialog">'+
-                  '<div class="modal-content">'+
-                    '<div class="modal-body">'+
-                      //'<button type="button" class="close" data-dismiss="modal">&times;</button>'+
-                      '<p>Add any item that is within</p>'+
-                      '<div class="row">'+
-                        '<div class="col-sm-10">'+
-                          '<div class="col-sm-5">'+
-                            '<input id="distanceQuantity" type="text" class="form-control" aria-label="..." placeholder="Enter distance">'+
-                          '</div>'+
-                          '<div class="col-sm-5">'+
-                            '<div class="btn-group">'+
-                              '<button id="distanceUnits" type="button" class="btn btn-default btn-md dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-                                'Select unit...<span class="caret"></span></button>'+
-                              '<ul class="dropdown-menu">'+
-                                '<li><a>miles</a></li>'+
-                                '<li><a>kilometers</a></li>'+
-                              '</ul>'+
-                            '</div>'+ // distance dropdown
-                          '</div>'+
-                        '</div>'+
-                      '</div>'+ // row
-                      '<p>of these coordinates:</p>'+
-                      '<div class="row">'+
-                        '<div class="col-sm-10">'+
-                          '<div class="col-sm-5">'+
-                            '<input id="latitude" type="text" class="form-control" aria-label="..." placeholder="Latitude (decimal)">'+
-                          '</div>'+
-                          '<div class="col-sm-5">'+
-                            '<input id="longitude" type="text" class="form-control" aria-label="..." placeholder="Longitude (decimal)">'+
-                          '</div>'+
-                        '</div>'+
-                      '</div>'+ // row
-                    '</div>'+ // modal body
-                    '<div class="modal-footer">'+
-                      '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>'+
-                      '<button id="distanceDone" type="button" class="btn btn-primary" data-dismiss="modal">Done</button>'+
-                  '  </div>'+
+      //
+      // Modal for items by Distance
+      //
+      '<div id="modalByDistance" class="modal fade caption_font" role="dialog">'+
+        '<div class="modal-dialog">'+
+          '<div class="modal-content">'+
+            '<div class="modal-body">'+
+              //'<button type="button" class="close" data-dismiss="modal">&times;</button>'+
+              '<p>Add any item that is within</p>'+
+              '<div class="row">'+
+                '<div class="col-sm-10">'+
+                  '<div class="col-sm-5">'+
+                    '<input id="distanceQuantity" type="text" class="form-control" aria-label="..." placeholder="Enter distance">'+
+                  '</div>'+
+                  '<div class="col-sm-5">'+
+                    '<div class="btn-group">'+
+                      '<button id="distanceUnits" type="button" class="btn btn-default btn-md dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+                        'Select unit...<span class="caret"></span></button>'+
+                      '<ul class="dropdown-menu">'+
+                        '<li><a>miles</a></li>'+
+                        '<li><a>kilometers</a></li>'+
+                      '</ul>'+
+                    '</div>'+ // distance dropdown
                   '</div>'+
                 '</div>'+
-              '</div>';
+              '</div>'+ // row
+              '<p>of these coordinates:</p>'+
+              '<div class="row">'+
+                '<div class="col-sm-10">'+
+                  '<div class="col-sm-5">'+
+                    '<input id="latitude" type="text" class="form-control" aria-label="..." placeholder="Latitude (decimal)">'+
+                  '</div>'+
+                  '<div class="col-sm-5">'+
+                    '<input id="longitude" type="text" class="form-control" aria-label="..." placeholder="Longitude (decimal)">'+
+                  '</div>'+
+                '</div>'+
+              '</div>'+ // row
+            '</div>'+ // modal body
+            '<div class="modal-footer">'+
+              '<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>'+
+              '<button id="distanceDone" type="button" class="btn btn-primary" data-dismiss="modal">Done</button>'+
+          '  </div>'+
+          '</div>'+
+        '</div>'+
+      '</div>';
 
 
 
@@ -259,14 +266,19 @@
 
 
 
-
-
         // store 'items by type' modal content-type
         $('#typeDone').click(function(){
           scalarLensObject["components"][0]["content-selector"]["content-type"] = $('#byType').val().split(/[_\s]/).join("-").toLowerCase();
           console.log(scalarLensObject);
+
+          // JSON updates content button text
+          let updateByType = scalarLensObject["components"][0]["content-selector"]["content-type"];
+          $('#content-selector-button').text('"' + updateByType + '"').append('<span class="caret"></span>');
+
           me.saveLens();
         });
+
+
 
         // store 'items by distance' values
         $('#distanceDone').click(function(){
@@ -277,7 +289,17 @@
           // coordinates
           scalarLensObject["components"][0]["content-selector"]["coordinates"] = $('#latitude').val() + ', ' + $('#longitude').val() ;
 
+
+          // JSON updates content button text
+          let updateByDistance =
+            scalarLensObject["components"][0]["content-selector"]["quantity"] + ' ' +
+            scalarLensObject["components"][0]["content-selector"]["units"] + ' from ' +
+            scalarLensObject["components"][0]["content-selector"]["coordinates"];
+
+          $('#content-selector-button').text('items ≤ ' + updateByDistance + '').append('<span class="caret"></span>');
+
           console.log(scalarLensObject);
+
           me.saveLens();
         });
 
@@ -286,9 +308,11 @@
 
 
 
-
+      // append html when plugin is called
       $("[property|='sioc:content']").append(lensHtml);
 
+
+      // ajax call
       ScalarLenses.prototype.saveLens = function() {
         this.baseURL = $('link#parent').attr('href');
         $.ajax({
@@ -308,6 +332,8 @@
         });
       }
 
+
+
       //
       /// callback for content-selector
       //
@@ -316,19 +342,22 @@
           //console.log(nodes);
 
           // extract the 'slug' property of each node and put into an array;
-          // this will be what gets stored in the "items" property
+          // this gets stored in the "items" property
           let contentItem = nodes.map(node => node.slug);
           //let contentItemsArray = JSON.stringify(contentItem);
           scalarLensObject["components"][0]["content-selector"]["items"] = contentItem;
 
           // show content selections in content-selector button
           let nodeTitle = nodes.map(node => node.title);
+
           if(nodeTitle.length <= 1){
             $('#content-selector-button').text('"'+ nodeTitle + '"').append('<span class="caret"></span>');
           } else if(nodeTitle.length > 1){
             $('#content-selector-button').text('"'+ nodeTitle[0] + '"' + ' and more...').append('<span class="caret"></span>');
           }
+
           console.log(scalarLensObject);
+
           me.saveLens();
 
         }
@@ -350,8 +379,8 @@
 
 
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
+    // plugin wrapper around the constructor,
+    // to prevent against multiple instantiations
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
