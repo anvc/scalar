67ad8c9a1614f5590bc0682e51389da86f63477f
Erik Loyer <erik@song.nu>
2020-06-15 15:43:27 -0700

    Prep work for lens manager

--- system/application/views/melons/cantaloupe/js/scalarlensmanager.jquery.js
+++ system/application/views/melons/cantaloupe/js/scalarlensmanager.jquery.js
@@ -1,1 +1,46 @@
-// lens manager plugin will go here
+/*!
+ * Scalar Lens manager plugin
+ */
+
+;(function ( $, window, document, undefined ) {
+
+    const pluginName = 'ScalarLensManager', defaults = {};
+
+    function ScalarLensManager(element, options) {
+        this.element = element;
+        this.options = $.extend( {}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    ScalarLensManager.prototype.init = function () {
+      this.bookId = $('link#book_id').attr('href');
+      this.userLevel = 'unknown';
+      if ($('link#user_level').length > 0) {
+        this.userLevel = $('link#user_level').attr('href');
+      }
+      this.userId = 'unknown';
+      if ($('link#logged_in').length > 0) {
+        this.userId = $('link#logged_in').attr('href');
+      }
+      // get all lenses in the book: http://[scalar_install]/system/lenses?book_id=XX
+    }
+
+    // plugin wrapper around the constructor,
+    // to prevent against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+      return this.each(function () {
+        if (!$.data(this, 'plugin_' + pluginName)) {
+          $.data(this, 'plugin_' + pluginName,
+          new ScalarLensManager( this, options ));
+        }
+      });
+    }
+
+})( jQuery, window, document );
+
+$(function(){
+    lensManager = $('#lens-manager').ScalarLensManager({});
+});
--- system/application/views/melons/cantaloupe/lenses.php
+++ system/application/views/melons/cantaloupe/lenses.php
@@ -37,27 +37,29 @@
 			<heading class="heading_font">
 				<h1 class="clearboth heading_weight">Lenses</h1>
 			</heading>
-			<div class="private-lenses">
-				<h3 class="heading_font heading_weight">My Private Lenses</h3>
-				<ul class="private-lenses-list">
-					<li class="caption_font"><a>Recently Viewed</a> <span class="badge dark caption_font">20</span> <span class="viz-icon list"></span></li>
-					<li class="caption_font"><a>recent stuff on the frontier</a> <span class="badge dark caption_font">80</span> <span class="viz-icon force"></span></li>
-				</ul>
-				<button type="button" class="btn btn-primary btn-xs caption_font">Add lens</button>
-			</div>
-			<div class="submitted-lenses">
-				<h3 class="heading_font heading_weight">My Submitted Lenses</h3>
-				<ul class="private-lenses-list">
-					<li class="caption_font"><a>recent stuff on the frontier</a> <span class="badge dark caption_font">8</span> <span class="viz-icon list"></span></li>
-				</ul>
-			</div>
-			<div class="author-lenses">
-				<h3 class="heading_font heading_weight">Author Lenses</h3>
-				<ul class="author-lenses-list">
-					<li class="caption_font"><a>Tokyo Area Commercial Networks</a> <span class="badge dark caption_font">10</span> <span class="viz-icon map"></span></li>
-					<li class="caption_font"><a>Consuming Places</a> <span class="badge dark caption_font">67</span> <span class="viz-icon tree"></span></li>
-					<li class="caption_font"><a>Contested Borderlands</a><span class="badge dark caption_font">40</span> <span class="viz-icon force"></span></li>
-				</ul>
+			<div id="lens-manager">
+				<div class="private-lenses">
+					<h3 class="heading_font heading_weight">My Private Lenses</h3>
+					<ul class="private-lenses-list">
+						<li class="caption_font"><a>Recently Viewed</a> <span class="badge dark caption_font">20</span> <span class="viz-icon list"></span></li>
+						<li class="caption_font"><a>recent stuff on the frontier</a> <span class="badge dark caption_font">80</span> <span class="viz-icon force"></span></li>
+					</ul>
+					<button type="button" class="btn btn-primary btn-xs caption_font">Add lens</button>
+				</div>
+				<div class="submitted-lenses">
+					<h3 class="heading_font heading_weight">My Submitted Lenses</h3>
+					<ul class="private-lenses-list">
+						<li class="caption_font"><a>recent stuff on the frontier</a> <span class="badge dark caption_font">8</span> <span class="viz-icon list"></span></li>
+					</ul>
+				</div>
+				<div class="author-lenses">
+					<h3 class="heading_font heading_weight">Author Lenses</h3>
+					<ul class="author-lenses-list">
+						<li class="caption_font"><a>Tokyo Area Commercial Networks</a> <span class="badge dark caption_font">10</span> <span class="viz-icon map"></span></li>
+						<li class="caption_font"><a>Consuming Places</a> <span class="badge dark caption_font">67</span> <span class="viz-icon tree"></span></li>
+						<li class="caption_font"><a>Contested Borderlands</a><span class="badge dark caption_font">40</span> <span class="viz-icon force"></span></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 		<div class="col-sm-8 heading_font">
