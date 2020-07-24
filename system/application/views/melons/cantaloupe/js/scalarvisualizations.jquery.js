410e72aa91dd705dd7a630259738154eebeccb00
Erik Loyer <erik@song.nu>
2020-07-24 16:09:29 -0700

    Troubleshooting

--- system/application/views/melons/cantaloupe/js/scalarvisualizations.jquery.js
+++ system/application/views/melons/cantaloupe/js/scalarvisualizations.jquery.js
@@ -1364,6 +1364,7 @@ window.scalarvis = { instanceCount: -1 };
       n = newSortedNodes.length;
       for (i = (n - 1); i >= 0; i--) {
         node = newSortedNodes[i];
+        console.log('b');
         base.processNode(node);
         index = base.sortedNodes.indexOf(node);
 
@@ -1794,6 +1795,7 @@ window.scalarvis = { instanceCount: -1 };
             o = nodes.length;
             for (j = 0; j < o; j++) {
               destNode = nodes[j];
+              console.log('a');
               base.processNode(destNode);
               destData = {
                 title: destNode.title,
