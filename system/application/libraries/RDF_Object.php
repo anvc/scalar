e8e0060809c5e4b0b0d91b746f9fbd633fe04845
Craig Dietrich <craigdietrich@gmail.com>
2020-09-22 17:00:33 -0700

    Correct typo keeping 3d annotation from showing up in RDF outputs

--- system/application/libraries/RDF_Object.php
+++ system/application/libraries/RDF_Object.php
@@ -54,7 +54,7 @@ class RDF_Object {
 	public $ns = array();
 	protected $version_cache = array();
 	protected $user_cache = array();
-	protected $rel_fields = array('sort_number','start_seconds','end_seconds','start_line_num','end_line_num','points','position-3d','additional','datetime','paragraph_num');
+	protected $rel_fields = array('sort_number','start_seconds','end_seconds','start_line_num','end_line_num','points','position_3d','additional','datetime','paragraph_num');
 	protected $defaults = array(
 		'books'			=> null,
 		'book'			=> null,
