bd83df96e565e5d075185bfabfa07ea4491e1b7d
Craig Dietrich <craigdietrich@gmail.com>
2021-09-30 17:19:22 -0700

    Remove deprecation warning

--- system/application/rdf/proxy.php
+++ system/application/rdf/proxy.php
@@ -90,7 +90,7 @@ function stripInvalidXml($value) {  // http://stackoverflow.com/questions/346603
     }
     $length = strlen($value);
     for ($i=0; $i < $length; $i++) {
-        $current = ord($value{$i});
+        $current = ord($value[$i]);
         if (($current == 0x9) ||
             ($current == 0xA) ||
             ($current == 0xD) ||
