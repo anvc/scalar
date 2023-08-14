49a37148a151be813bc1c64dd9408433b54a320b
Erik Loyer <erikcloyer@gmail.com>
2023-08-14 08:51:44 -0700

    Remove log

--- system/application/views/melons/cantaloupe/js/scalarvisualizations.jquery.js
+++ system/application/views/melons/cantaloupe/js/scalarvisualizations.jquery.js
@@ -477,7 +477,6 @@ window.scalarvis = { instanceCount: -1 };
 
     base.getLensResults = function(success) {
       base.options.lens.book_urn = 'urn:scalar:book:' + $('link#book_id').attr('href');
-      //console.log(JSON.stringify(base.options.lens.components, null, 2));
       let url = $('link#approot').attr('href').replace('application/', '') + 'lenses';
       base.loadingMsgShown = false;
       base.startTime = new Date();
